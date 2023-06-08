926ba3e5b21e0f7548e3ddd92d05dd8ff8f73af0
Sebastian <sebastian.flores@elaniin.com>
2023-06-07 22:42:55 -0600

    feat: :sparkles: Now logIn view is a component
    Migrate logIn view and made it a component

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,11 @@
+import LogIn from "@/components/LogIn";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-between ">
-      <div className="flex flex-col items-center justify-center w-[50%]">
-        <Image width={512} height={512} src={'/logIn.jpg'}/>
-        <h1 className=" text-4xl font-rubik font-bold">R y S Transportes</h1>
-      </div>
-      <div className="flex flex-col items-start pl-40 justify-center w-[50%] bg-[#9DD9D2] min-h-screen">
-        <h2 className="font-bold text-[#3E4856] font-rubik text-4xl text-left pb-12">Inicia sesión</h2>
-        <form className="flex flex-col items-start justify-center w-[50%]">
-          <label className=" font-medium pb-4">
-            Correo
-          </label>
-          <input className=" rounded-lg py-2 px-2 w-full" placeholder="example@email.com">
-          </input>
-          <label className=" font-medium pt-8 pb-4">
-            Contraseña
-          </label>
-          <input className=" rounded-lg py-2 px-2 w-full" placeholder="example@email.com">
-          </input>
-          <button className="mt-20 py-4 text-center w-full font-medium font-rubik text-white bg-[#FF8811] rounded-lg">Inicia sesión</button>
-          <Link href="/" className="mt-4 w-full text-center font-medium font-rubik">olvide mi contraseña</Link>
-        </form>
-      </div>
+    <main>
+      <LogIn/>
     </main>
   );
 }
